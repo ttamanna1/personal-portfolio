f6c567c2ca4d4a9ec0ab2800d6ddae9a801d92f3
Tamanna <tamannakhanum@hotmail.co.uk>
2024-02-08 10:36:00 +0000

    "Created and styled skill section."

--- personal-portfolio/src/components/Skills.jsx
+++ personal-portfolio/src/components/Skills.jsx
@@ -1,8 +1,37 @@
 
 
 const Skills = () => {
+
+  const skills = [
+    'HTML',
+    'CSS/Sass',
+    'JavaScript',
+    'Python',
+    'Node',
+    'PostgreSQL',
+    'Django',
+    'MongoDB',
+    'Github',
+    'React',
+    'Express',
+    'Insomnia',
+    'NPM',
+    'Bootstrap',
+    'Heroku',
+    'Netlify'
+  ]
+
   return (
-    <div>Skills</div>
+    <section className="skills-section">
+      <h2 className="title">Skills</h2>
+      <ul className="skills-list">
+        {skills.map((skill, key) => (
+          <li key={key} className="skill-item skill-btn">
+            {skill}
+          </li>
+        ))}
+      </ul>
+    </section>
   )
 }
 
