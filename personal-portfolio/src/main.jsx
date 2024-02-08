cefb6b0336841c66907ae5b3200659cd5e15ea86
Tamanna <tamannakhanum@hotmail.co.uk>
2024-02-08 07:20:57 +0000

    "Added router."

--- personal-portfolio/src/main.jsx
+++ personal-portfolio/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//? Components
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
