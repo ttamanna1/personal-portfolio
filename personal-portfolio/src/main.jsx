bc597b96debb7f1b62e1e519e04d7c3ba27005a5
Tamanna <tamannakhanum@hotmail.co.uk>
2024-02-08 07:25:58 +0000

    "Added scss styles files and imported to main."

--- personal-portfolio/src/main.jsx
+++ personal-portfolio/src/main.jsx
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+//? Styles
+import './styles/main.scss'
+
 //? Components
 import App from './App.jsx'
 
