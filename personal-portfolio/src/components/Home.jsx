6a309a3bfdbfe7f7e1b19178372a7640c5e8a680
Tamanna <tamannakhanum@hotmail.co.uk>
2024-02-08 15:34:08 +0000

    "Updated description about myself."

--- personal-portfolio/src/components/Home.jsx
+++ personal-portfolio/src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
       <h1>Hi, I&apos;m <span>Tamanna Khanum</span></h1>
       <img src={DP} alt='Tamanna Khanum'/>
       <h2>Junior Software Developer</h2>
-      <p>With a background in community pharmacy, I am taking a deep dive into creating digital solutions. <br/> Watch this space.</p>
+      <p>With a background in community pharmacy, I am taking a deep dive into creating digital solutions. After taking a couple of courses in web development with Code First Girls, I was ready to learn more about the digital space, which led me to study full-time with General Assembly in the Software Engineering Immersive course. I am ready to embark on this trajectory in my career and I can&apos;t wait to get stuck in.<br/> Watch this space.</p>
       <div className='social-links'>
         <Link target='_blank' to='https://github.com/ttamanna1' className='link link-icons'><GitHubIcon className='icon'/></Link>
         <Link target='_blank' to='https://linkedin.com/in/tamanna-khanum' className='link link-icons'><LinkedInIcon className='icon'/></Link>
