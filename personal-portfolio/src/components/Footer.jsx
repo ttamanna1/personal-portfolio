fb7b15d118e6ffa9fb9c011a7893d712ea04e860
Tamanna <tamannakhanum@hotmail.co.uk>
2024-04-25 09:45:37 +0100

    "Added social links to footer."

--- personal-portfolio/src/components/Footer.jsx
+++ personal-portfolio/src/components/Footer.jsx
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom'
+
+//? Icons
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <a target="_blank" rel="noopener noreferrer" href='https://github.com/ttamanna1' className="footer-link">Tamanna Khanum {new Date().getFullYear()}</a>
+      <div className="footer-link">Tamanna Khanum {new Date().getFullYear()}</div>
+      <div className='social-links'>
+        <Link target='_blank' to='https://github.com/ttamanna1' className='link link-icons'><GitHubIcon className='icon'/></Link>
+        <Link target='_blank' to='https://linkedin.com/in/tamanna-khanum' className='link link-icons'><LinkedInIcon className='icon'/></Link>
+      </div>
     </footer>
   )
 }
