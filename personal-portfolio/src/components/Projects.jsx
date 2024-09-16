f1c74bc1b7e8d51ac3fa2054f1871c637f527b24
Tamanna <tamannakhanum@hotmail.co.uk>
2024-09-16 15:49:38 +0100

    "Updated project section. Added a project which I have not yet completed."

--- personal-portfolio/src/components/Projects.jsx
+++ personal-portfolio/src/components/Projects.jsx
@@ -5,6 +5,7 @@ import bookshelf from '../assets/bookshelf.png'
 import tasty from '../assets/tasty.png'
 import hp from '../assets/hp.png'
 import baby from '../assets/baby.png'
+import coming from '../assets/coming.png'
 
 //? Icons
 import LaunchIcon from '@mui/icons-material/Launch'
@@ -13,8 +14,16 @@ import GitHubIcon from '@mui/icons-material/GitHub'
 const Projects = () => {
   const projects = [
     {
+      title: 'COMING SOON- Trivia Web App',
+      description: 'Fun frontend project using a third party API to create a Trivia game.',
+      image: coming,
+      skills: ['HTML', 'Sass', 'TypeScript', 'React'],
+      // live: '',
+      github: 'https://github.com/ttamanna1/Trivia-App'
+    },
+    {
       title: 'bookshelf',
-      description: 'Solo project | 1 week | I created a reading list tracker with CRUD functionality using a Python Django API and Django REST framework. Users can register and login for an account and move their book entries between their reading lists.',
+      description: 'I created a reading list tracker with CRUD functionality using a Python Django API and Django REST framework. Users can register and login for an account and move their book entries between their reading lists.',
       image: bookshelf,
       skills: ['HTML', 'Sass', 'React', 'Python', 'Django', 'PostgreSQL', 'Heroku'],
       live: 'https://bookshelf-project-5fec0e408021.herokuapp.com/',
@@ -22,7 +31,7 @@ const Projects = () => {
     },
     {
       title: 'The Tasty Palette',
-      description: 'Group project (4 people) | 1 week | We created a recipe sharing app with CRUD functionality where users can also leave reviews of the recipes. I focused on working on the recipe index component with filtering and the user profile page.',
+      description: 'Group project (4 people) | We created a recipe sharing app with CRUD functionality where users can also leave reviews of the recipes. I focused on working on the recipe index component with filtering and the user profile page.',
       image: tasty,
       skills: ['HTML', 'Sass', 'React', 'Node', 'Express', 'MongoDB', 'Bootstrap', 'Heroku'],
       live: 'https://the-tasty-pallete-f0000d235851.herokuapp.com/',
@@ -30,7 +39,7 @@ const Projects = () => {
     },
     {
       title: 'Harry Potter App',
-      description: 'Pair project | 2 days | React frontend project: We created a Harry Potter app displaying character cards and spells using a 3rd party API.',
+      description: 'Pair project | React frontend project: We created a Harry Potter app displaying character cards and spells using a 3rd party API.',
       image: hp,
       skills: ['HTML', 'Sass', 'React', 'Bootstrap', 'Netlify'],
       live: 'https://hpwebapp.netlify.app',
@@ -38,7 +47,7 @@ const Projects = () => {
     },
     {
       title: 'Baby\'s Day Out',
-      description: 'Solo project | 1 week | I created a Frogger- style game, using vanilla JavaScript, with a theme inspired by the Baby’s Day Out movie.',
+      description: 'I created a Frogger- style game, using vanilla JavaScript, with a theme inspired by the Baby’s Day Out movie.',
       image: baby,
       skills: ['HTML', 'CSS', 'JavaScript'],
       live: 'https://ttamanna1.github.io/Babys-Day-Out/',
